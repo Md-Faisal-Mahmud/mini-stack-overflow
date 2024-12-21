cec82e73a13e5100e48f9a174fd72826a589eeb7
md-faisal-mahmud <md.faisal.mahmud.se@gmail.com>
2024-12-21 17:12:44 +0600

    added vote controller and webModule and WebProfile and fixed error

---
+++ src/MiniStackOverflow.Web/Models/VoteModel.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using MiniStackOverflow.Application.Features.Training.Services;
+
+namespace MiniStackOverflow.Web.Models
+{
+    public class VoteModel
+    {
+        private ILifetimeScope _scope;
+        private IVoteManagementService _voteManagementService;
+
+        public Guid UserId { get; set; }
+        public int VoteType { get; set; }
+        public Guid QuestionId { get; set; }
+
+        public VoteModel() { }
+
+        public VoteModel(IVoteManagementService voteManagementService)
+        {
+            _voteManagementService = voteManagementService;
+        }
+
+        internal void Resolve(ILifetimeScope scope)
+        {
+            _scope = scope;
+            _voteManagementService = _scope.Resolve<IVoteManagementService>();
+        }
+
+        public async Task CreateVoteAsync()
+        {
+            await _voteManagementService.CreateVoteAsync(QuestionId, UserId, VoteType);
+        }
+    }
+}
---
+++ src/MiniStackOverflow.Web/WebProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MiniStackOverflow.Domain.Entities;
+using MiniStackOverflow.Web.Models;
+
+namespace StackOverflow.Web.Profiles
+{
+    public class WebProfile : Profile
+    {
+        public WebProfile()
+        {
+            CreateMap<ProfileViewModel, User>()
+                .ReverseMap();
+
+            CreateMap<ProfileEditModel, User>()
+                .ReverseMap();
+
+            CreateMap<QuestionDetailsModel, Question>()
+                .ReverseMap();
+        }
+    }
+}
