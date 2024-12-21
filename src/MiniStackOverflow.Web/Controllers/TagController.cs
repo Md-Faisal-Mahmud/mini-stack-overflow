610394aeab01f1faf0c9e79d5ea3f80980479514
md-faisal-mahmud <md.faisal.mahmud.se@gmail.com>
2024-12-21 17:06:41 +0600

    added tag controller

---
+++ src/MiniStackOverflow.Web/Controllers/TagController.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Mvc;
+using MiniStackOverflow.Infrastructure;
+using MiniStackOverflow.Web.Models;
+
+
+namespace StackOverflow.Web.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ILifetimeScope _scope;
+
+        public TagController(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+        [HttpPost]
+        public async Task<JsonResult> GetTags(TagListModel model)
+        {
+            var dataTablesModel = new DataTablesAjaxRequestUtility(Request);
+            model.Resolve(_scope);
+            var data = await model.GetPagedTagsAsync(dataTablesModel);
+            return Json(data);
+        }
+    }
+}
---
+++ src/MiniStackOverflow.Web/Models/TagListModel.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using MiniStackOverflow.Application.Features.Training.Services;
+using MiniStackOverflow.Infrastructure;
+using System.Web;
+
+namespace MiniStackOverflow.Web.Models
+{
+    public class TagListModel
+    {
+        private ILifetimeScope _scope;
+        private ITagManagementService _tagManagementService;
+        public TagListModel()
+        {
+        }
+        public TagListModel(ITagManagementService tagManagementService)
+        {
+            _tagManagementService = tagManagementService;
+        }
+        internal void Resolve(ILifetimeScope scope)
+        {
+            _scope = scope;
+            _tagManagementService = _scope.Resolve<ITagManagementService>();
+        }
+        public async Task<object> GetPagedTagsAsync(DataTablesAjaxRequestUtility dataTablesUtility)
+        {
+            var data = await _tagManagementService.GetPagedTagsAsync(
+                dataTablesUtility.PageIndex,
+                dataTablesUtility.PageSize,
+                dataTablesUtility.SearchText,
+                dataTablesUtility.GetSortText(new string[] { "TagName" })
+                );
+
+            return new
+            {
+                recordsTotal = data.total,
+                recordsFiltered = data.totalDisplay,
+                data = (from record in data.records
+                        select new string[]
+                        {
+                    HttpUtility.HtmlEncode(record.TagName),
+                    record.QuestionId.ToString(),
+                    record.Id.ToString()
+                        }
+                ).ToArray()
+            };
+        }
+    }
+}
